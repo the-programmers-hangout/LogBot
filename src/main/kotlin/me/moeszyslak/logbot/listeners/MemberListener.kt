ba028fc5daed874e75dbbc158aa60de633e86ba2
moe <moe9999@protonmail.com>
2020-11-15 19:32:36 +0000

    Honor config for tracking member join/leave

--- src/main/kotlin/me/moeszyslak/logbot/listeners/MemberListener.kt
+++ src/main/kotlin/me/moeszyslak/logbot/listeners/MemberListener.kt
@@ -3,14 +3,25 @@ package me.moeszyslak.logbot.listeners
 import com.gitlab.kordlib.core.event.guild.MemberJoinEvent
 import com.gitlab.kordlib.core.event.guild.MemberLeaveEvent
 import me.jakejmattson.discordkt.api.dsl.listeners
+import me.moeszyslak.logbot.dataclasses.Configuration
 import me.moeszyslak.logbot.services.LoggerService
 
-fun memberListener(loggerService: LoggerService) = listeners {
+fun memberListener(loggerService: LoggerService, configuration: Configuration) = listeners {
     on<MemberJoinEvent> {
-        loggerService.memberJoin(getGuild(), member)
+        val guild = getGuild()
+        val guildConfig = configuration[guild.id.longValue] ?: return@on
+
+        if (!guildConfig.trackMembers) return@on
+
+        loggerService.memberJoin(guild, member)
     }
 
     on<MemberLeaveEvent> {
-        loggerService.memberLeave(getGuild(), user)
+        val guild = getGuild()
+        val guildConfig = configuration[guild.id.longValue] ?: return@on
+
+        if (!guildConfig.trackMembers) return@on
+
+        loggerService.memberLeave(guild, user)
     }
 }
