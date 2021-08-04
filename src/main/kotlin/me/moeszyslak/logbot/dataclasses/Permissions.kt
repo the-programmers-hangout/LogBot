7455651b918e056e0a32b37a6be7ff9d07dc2277
moe <moe9999@protonmail.com>
2021-08-04 12:52:03 +0100

    Permission fix

--- src/main/kotlin/me/moeszyslak/logbot/dataclasses/Permissions.kt
+++ src/main/kotlin/me/moeszyslak/logbot/dataclasses/Permissions.kt
@@ -4,7 +4,6 @@ import dev.kord.common.entity.Permission
 import dev.kord.core.any
 import me.jakejmattson.discordkt.api.dsl.PermissionContext
 import me.jakejmattson.discordkt.api.dsl.PermissionSet
-import me.jakejmattson.discordkt.api.extensions.toSnowflake
 
 enum class Permissions : PermissionSet {
     BOT_OWNER {
@@ -19,14 +18,6 @@ enum class Permissions : PermissionSet {
             return member.isOwner()
         }
     },
-    STAFF {
-        override suspend fun hasPermission(context: PermissionContext): Boolean {
-            val guild = context.guild ?: return false
-            val member = context.user.asMember(guild.id)
-            val configuration = context.discord.getInjectionObjects<Configuration>()
-            return member.roles.any { it.id.value == configuration[guild.id.value]?.staffRole}
-        }
-    },
     ADMINISTRATOR {
         override suspend fun hasPermission(context: PermissionContext): Boolean {
             val guild = context.guild ?: return false
@@ -36,6 +27,14 @@ enum class Permissions : PermissionSet {
                 .contains(
                     Permission.Administrator
                 )
+        }
+    },
+    STAFF {
+        override suspend fun hasPermission(context: PermissionContext): Boolean {
+            val guild = context.guild ?: return false
+            val member = context.user.asMember(guild.id)
+            val configuration = context.discord.getInjectionObjects<Configuration>()
+            return member.roles.any { it.id.value == configuration[guild.id.value]?.staffRole}
         }
     },
     NONE {
