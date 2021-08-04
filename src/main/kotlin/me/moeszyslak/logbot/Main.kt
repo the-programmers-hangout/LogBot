38c4874bf8213b40d72308b1f441888be5028fd2
moe <moe9999@protonmail.com>
2021-08-04 12:52:19 +0100

    Version bump

--- src/main/kotlin/me/moeszyslak/logbot/Main.kt
+++ src/main/kotlin/me/moeszyslak/logbot/Main.kt
@@ -88,7 +88,7 @@ suspend fun main() {
 
                 name = "Bot Info"
                 value = "```" +
-                        "Version: 1.1.2\n" +
+                        "Version: 1.1.3\n" +
                         "DiscordKt: ${versions.library}\n" +
                         "Kord: ${versions.kord}\n" +
                         "Kotlin: ${versions.kotlin}" +
