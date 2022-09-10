661f4c56e1ea590551306fd17b4c096e9442a6b5
Jake Mattson <dev.ioparameters@simplelogin.fr>
2022-09-10 13:20:22 -0500

    Disable recommendCommands

--- src/main/kotlin/me/moeszyslak/logbot/Main.kt
+++ src/main/kotlin/me/moeszyslak/logbot/Main.kt
@@ -3,15 +3,13 @@ package me.moeszyslak.logbot
 import dev.kord.common.annotation.KordPreview
 import dev.kord.common.entity.Permission
 import dev.kord.common.entity.Permissions
-import dev.kord.core.supplier.EntitySupplyStrategy
 import dev.kord.gateway.Intent
 import dev.kord.gateway.PrivilegedIntent
 import me.jakejmattson.discordkt.dsl.bot
-import me.jakejmattson.discordkt.extensions.*
+import me.jakejmattson.discordkt.extensions.plus
 import me.moeszyslak.logbot.dataclasses.Configuration
 import me.moeszyslak.logbot.services.LoggerService
 import java.awt.Color
-import java.time.Instant
 
 @PrivilegedIntent
 @KordPreview
@@ -19,15 +17,14 @@ suspend fun main() {
     val token = System.getenv("BOT_TOKEN") ?: null
 
     bot(token) {
-        val configuration = data("config/config.json") { Configuration() }
+        data("config/config.json") { Configuration() }
 
-        prefix {
-            "/"
-        }
+        prefix { "/" }
 
         configure {
             theme = Color.MAGENTA
             commandReaction = null
+            recommendCommands = false
             defaultPermissions = Permissions(Permission.ManageMessages)
             intents = Intent.GuildMembers + Intent.GuildVoiceStates + Intent.GuildMessageReactions + Intent.DirectMessagesReactions
         }
